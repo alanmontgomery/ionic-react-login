418f485a9daaec80ee5d8037ca659bc8ae616b54
minhpb-scuti <minhpb@scuti.asia>
2022-07-02 12:03:02 +0700

    check email password in login screen

--- src/data/utils.js
+++ src/data/utils.js
@@ -1,46 +1,42 @@
 import { useState } from "react";
 
 export const useFormInput = (initialValue = "") => {
+  const [value, setValue] = useState(initialValue);
 
-    const [ value, setValue ] = useState(initialValue);
-    
-    const handleChange = async e => {
+  const handleChange = async (e) => {
+    const tempValue = await e.currentTarget.value;
+    setValue(tempValue);
+  };
 
-        const tempValue = await e.currentTarget.value;
-        setValue(tempValue);
+  return {
+    value,
+    reset: (newValue) => setValue(newValue),
+    onIonChange: handleChange,
+    onKeyUp: handleChange,
+  };
+};
+
+export const validateForm = (fields) => {
+  let errors = [];
+
+  fields.forEach((field) => {
+    if (field.required) {
+      const fieldValue = field.input.state.value;
+      const fieldLabel = field.id;
+      if (
+        fieldValue === "" ||
+        (fieldLabel === "email" && fieldValue !== "minhhieu@scuti.asia") ||
+        (fieldLabel === "password" && fieldValue !== "123456789") ||
+        (fieldLabel === "password" && fieldValue === "")
+      ) {
+        const error = {
+          id: fieldLabel,
+          message: `Please check your ${fieldLabel}`,
+        };
+        errors.push(error);
+      }
     }
+  });
 
-    return {
-
-        value,
-        reset: (newValue) => setValue(newValue),
-        onIonChange: handleChange,
-        onKeyUp: handleChange
-    };
-}
-
-export const validateForm = fields => {
-
-	let errors = [];
-
-	fields.forEach(field => {
-
-		if (field.required) {
-
-			const fieldValue = field.input.state.value;
-
-			if (fieldValue === "") {
-
-				const error = {
-
-					id: field.id,
-					message: `Please check your ${ field.id }`,
-				};
-
-				errors.push(error);
-			}
-		}
-	});
-
-	return errors;
-}
+  return errors;
+};
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,90 +1,106 @@
-import { IonBackButton, IonButton, IonButtons, IonCardTitle, IonCol, IonContent, IonFooter, IonGrid, IonHeader, IonIcon, IonPage, IonRouterLink, IonRow, IonToolbar } from '@ionic/react';
-import styles from './Login.module.scss';
+import {
+  IonBackButton,
+  IonButton,
+  IonButtons,
+  IonCardTitle,
+  IonCol,
+  IonContent,
+  IonFooter,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonRow,
+  IonToolbar,
+} from "@ionic/react";
+import styles from "./Login.module.scss";
 
 import { arrowBack, shapesOutline } from "ionicons/icons";
-import CustomField from '../components/CustomField';
-import { useLoginFields } from '../data/fields';
-import { Action } from '../components/Action';
-import { Wave } from '../components/Wave';
-import { useEffect, useState } from 'react';
-import { validateForm } from '../data/utils';
-import { useParams } from 'react-router';
+import CustomField from "../components/CustomField";
+import { useLoginFields } from "../data/fields";
+import { Action } from "../components/Action";
+import { Wave } from "../components/Wave";
+import { validateForm } from "../data/utils";
+import { useHistory } from "react-router";
 
 const Login = () => {
-    
-    const params = useParams();
+  const history = useHistory();
 
-    const fields = useLoginFields();
-    const [ errors, setErrors ] = useState(false);
+  const fields = useLoginFields();
+  const [errors, setErrors] = useState(false);
 
-    const login = () => {
+  const login = () => {
+    const errors = validateForm(fields);
+    setErrors(errors);
 
-        const errors = validateForm(fields);
-        setErrors(errors);
-
-        if (!errors.length) {
-
-            //  Submit your form here
-        }
+    if (!errors.length) {
+      history.push("/home");
     }
+  };
 
-    useEffect(() => {
+  // useEffect(() => {
 
-        return () => {
+  //     return () => {
 
-            fields.forEach(field => field.input.state.reset(""));
-            setErrors(false);
-        }
-    }, [params]);
+  //         fields.forEach(field => field.input.state.reset(""));
+  //         setErrors(false);
+  //     }
+  // }, [params]);
 
-	return (
-		<IonPage className={ styles.loginPage }>
-			<IonHeader>
-				<IonToolbar>
-					
-                    <IonButtons slot="start">
-                        <IonBackButton icon={ arrowBack } text="" className="custom-back" />
-                    </IonButtons>
+  return (
+    <IonPage className={styles.loginPage}>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton icon={arrowBack} text="" className="custom-back" />
+          </IonButtons>
 
-                    <IonButtons slot="end">
-                        <IonButton className="custom-button">
-                            <IonIcon icon={ shapesOutline } />
-                        </IonButton>
-                    </IonButtons>
-				</IonToolbar>
-			</IonHeader>
-			<IonContent fullscreen>
-                <IonGrid className="ion-padding">
-                    <IonRow>
-                        <IonCol size="12" className={ styles.headingText }>
-                            <IonCardTitle>Log in</IonCardTitle>
-                            <h5>Welcome back, hope you're doing well</h5>
-                        </IonCol>
-                    </IonRow>
+          <IonButtons slot="end">
+            <IonButton className="custom-button">
+              <IonIcon icon={shapesOutline} />
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonGrid className="ion-padding">
+          <IonRow>
+            <IonCol size="12" className={styles.headingText}>
+              <IonCardTitle>Log in</IonCardTitle>
+              <h5>Welcome back, hope you're doing well</h5>
+            </IonCol>
+          </IonRow>
 
-                    <IonRow className="ion-margin-top ion-padding-top">
-                        <IonCol size="12">
+          <IonRow className="ion-margin-top ion-padding-top">
+            <IonCol size="12">
+              {fields.map((field) => {
+                return <CustomField field={field} errors={errors} />;
+              })}
 
-                            { fields.map(field => {
+              <IonButton
+                className="custom-button"
+                expand="block"
+                onClick={login}
+              >
+                Login
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
 
-                                return <CustomField field={ field } errors={ errors } />;
-                            })}
-
-                            <IonButton className="custom-button" expand="block" onClick={ login }>Login</IonButton>
-                        </IonCol>
-                    </IonRow>
-                </IonGrid>
-			</IonContent>
-
-			<IonFooter>
-				<IonGrid className="ion-no-margin ion-no-padding">
-
-                    <Action message="Don't have an account?" text="Sign up" link="/signup" />
-                    <Wave />
-				</IonGrid>
-			</IonFooter>
-		</IonPage>
-	);
+      <IonFooter>
+        <IonGrid className="ion-no-margin ion-no-padding">
+          <Action
+            message="Don't have an account?"
+            text="Sign up"
+            link="/signup"
+          />
+          <Wave />
+        </IonGrid>
+      </IonFooter>
+    </IonPage>
+  );
 };
 
 export default Login;
